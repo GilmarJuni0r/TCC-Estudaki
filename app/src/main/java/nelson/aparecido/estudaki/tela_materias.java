030ba43280cd12adcdd3efc30b5492fb4c2dbcc9
Nelson Aparecido <nelson.anre@gmail.com>
2021-05-17 20:25:41 -0300

    Start Commit!

--- app/src/main/java/nelson/aparecido/estudaki/tela_materias.java
+++ app/src/main/java/nelson/aparecido/estudaki/tela_materias.java
@@ -1,17 +1,46 @@
 package nelson.aparecido.estudaki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.jaeger.library.StatusBarUtil;
 
 public class tela_materias extends AppCompatActivity {
+
+            ConstraintLayout matematica;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_materias);
 
         StatusBarUtil.setTransparent(this);
+
+
+
+
+        matematica = findViewById(R.id.constaint_matematica);
+
+        matematica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), tela_materias_matematica.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
 
 
     }
---
+++ app/src/main/java/nelson/aparecido/estudaki/tela_materias_matematica.java
@@ -0,0 +1,22 @@
+package nelson.aparecido.estudaki;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jaeger.library.StatusBarUtil;
+
+public class tela_materias_matematica extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_materias_matematica);
+
+        StatusBarUtil.setTransparent(this);
+
+
+    }
+
+}
+
+
+
