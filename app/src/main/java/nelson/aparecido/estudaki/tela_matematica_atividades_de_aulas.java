8e02eae6592b04c7851b01c4cf03e5aede250212
Nelson Aparecido <nelson.anre@gmail.com>
2021-05-24 20:11:37 -0300

    2 telas novas, Atividades aulas, Materias Aulas, 1 imagem Nova, Atividade Corrigida,

---
+++ app/src/main/java/nelson/aparecido/estudaki/tela_matematica_atividades_de_aulas.java
@@ -0,0 +1,21 @@
+package nelson.aparecido.estudaki;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jaeger.library.StatusBarUtil;
+
+public class tela_matematica_atividades_de_aulas extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_matematica_atividades_de_aulas);
+
+        StatusBarUtil.setTransparent(this);
+
+
+
+    }
+
+}
