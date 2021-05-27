02076e1d0a0e70b1d8bd4ec733adcca6d4384822
gilmar <gilmaralmeidajr23@gmail.com>
2021-05-26 22:27:01 -0300

    TelaPerfilParcial

--- app/src/main/java/nelson/aparecido/estudaki/MainActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/MainActivity.java
@@ -1,20 +1,32 @@
 package nelson.aparecido.estudaki;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.jaeger.library.StatusBarUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView materias;
-    ImageView aulas;
+    private ImageView materias, aulas;
 
-    View calendario, lupa, home, professor, perfil;
+    private View calendario, lupa, home, professor, perfil;
+
+    private TextView txtNome;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
         StatusBarUtil.setTransparent(this);
 
-
+        txtNome = findViewById(R.id.txt_nome_principal);
         materias = findViewById(R.id.img_materias_telaprincipal);
-
-
         aulas = findViewById(R.id.img_aulas_telaprincipal);
-
-
 
         materias.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,27 +91,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        professor.setOnClickListener(new View.OnClickListener() {
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuario").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+
             @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(getApplicationContext(), TelaBatepapoProfessores.class);
-                startActivity(intent);
-
-
+            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot documentSnapshotMain, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                if(documentSnapshotMain != null){
+                    txtNome.setText(documentSnapshotMain.getString("nome"));
+                }
             }
         });
-
-
-        /*  FIM BARRA DE TAREFA */
-
-
-
-
-
-
-
-
     }
 }
--- app/src/main/java/nelson/aparecido/estudaki/TelaPerfil.java
+++ app/src/main/java/nelson/aparecido/estudaki/TelaPerfil.java
@@ -1,19 +1,33 @@
 package nelson.aparecido.estudaki;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.jaeger.library.StatusBarUtil;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class TelaPerfil extends AppCompatActivity {
 
 
     private View calendario, lupa, home, professor, perfil, btnLogout;
     private TextView txtNome, txtOcupacao, txtTurma, txtIdade, txtRaCpf, txtNomeProfessor, txtEscola, txtEmail;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String usuarioID;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +67,53 @@ public class TelaPerfil extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuario").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot documentSnapshotPerfil, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                if(documentSnapshotPerfil != null){
+                    String dataNascimento = documentSnapshotPerfil.getString("dataNascimento");
+
+                    txtNome.setText(documentSnapshotPerfil.getString("nome"));
+                    txtEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+                    txtIdade.setText("Idade: "+ calculaIdade(dataNascimento) + " anos");
+                    if(documentSnapshotPerfil.getString("ocupacao").equalsIgnoreCase("Professor") || documentSnapshotPerfil.getString("ocupacao").equalsIgnoreCase("Professora")){
+                        txtOcupacao.setText("Docente");
+                        txtTurma.setText("");
+                        txtRaCpf.setText("CPF: " + documentSnapshotPerfil.getString("cpf"));
+                        txtNomeProfessor.setText("");
+                        txtEscola.setText("");
+                    }else{
+                        txtOcupacao.setText("Estudante");
+                        txtTurma.setText("");
+                        txtRaCpf.setText("RA: " + documentSnapshotPerfil.getString("ra"));
+                        txtNomeProfessor.setText("");
+                        txtEscola.setText("");
+                    }
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected static int calculaIdade(String dataNascimento){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        LocalDate dataConvertida = LocalDate.parse(dataNascimento, formatter);
+        final LocalDate dataAtual = LocalDate.now();
+        final Period periodo = Period.between(dataConvertida, dataAtual);
+        return periodo.getYears();
+    }
+
     protected void inicializarComponentes(){
 
+        //Compenentes da tela
         btnLogout = findViewById(R.id.btn_logout);
 
         txtNome = findViewById(R.id.txt_nome);
