ceb48c6d822f9e6404774fe7efddb06075630e32
Karina Cruz <karinacss@outlook.com>
2021-05-29 22:20:06 -0300

    Conexao entre telas de Atividade, Main com a tela Me Ajuda

--- app/src/main/java/nelson/aparecido/estudaki/MainActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/MainActivity.java
@@ -20,11 +20,8 @@ import com.jaeger.library.StatusBarUtil;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView materias, aulas;
-
-    private View calendario, lupa, home, professor, perfil;
-
+    private View calendario, lupa, home, professor, perfil, btn_me_ajuda;
     private TextView txtNome;
-
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String usuarioID;
 
@@ -38,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         txtNome = findViewById(R.id.txt_nome_principal);
         materias = findViewById(R.id.img_materias_telaprincipal);
         aulas = findViewById(R.id.img_aulas_telaprincipal);
+
+        //Ir para tela Me Ajuda
+        btn_me_ajuda = (View) findViewById(R.id.view_me_ajuda_main);
+        btn_me_ajuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeAjudaActivity.class);
+                startActivity(intent);
+            }
+        });
 
         materias.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-                /* BARRA DE TAREFA */
+            /* BARRA DE TAREFA */
             calendario = findViewById(R.id.view_calendario);
             lupa = findViewById(R.id.view_lupa);
             home = findViewById(R.id.view_home);
@@ -72,55 +79,40 @@ public class MainActivity extends AppCompatActivity {
            perfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), PerfilActivity.class);
                 startActivity(intent);
-
-
             }
         });
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
-
-
             }
         });
 
         professor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), BatePapoActivity.class);
                 startActivity(intent);
-
-
             }
         });
 
         calendario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), CalendarioActivity.class);
                 startActivity(intent);
-
-
             }
         });
 
         lupa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplicationContext(), PesquisaActivity.class);
                 startActivity(intent);
-
-
             }
         });
 
--- app/src/main/java/nelson/aparecido/estudaki/MateriaAtividadeAulaActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/MateriaAtividadeAulaActivity.java
@@ -1,6 +1,8 @@
 package nelson.aparecido.estudaki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,13 +10,71 @@ import com.jaeger.library.StatusBarUtil;
 
 public class MateriaAtividadeAulaActivity extends AppCompatActivity {
 
+    private View calendario, lupa, home, professor, perfil, btn_me_ajuda;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_atividades);
 
         StatusBarUtil.setTransparent(this);
 
+        //Ir para tela Me Ajuda
+        btn_me_ajuda = (View) findViewById(R.id.view_me_ajuda_atividades);
+        btn_me_ajuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeAjudaActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        /* BARRA DE TAREFA */
+        calendario = findViewById(R.id.view_calendario);
+        lupa = findViewById(R.id.view_lupa);
+        home = findViewById(R.id.view_home);
+        professor = findViewById(R.id.view_conversa_professor);
+        perfil = findViewById(R.id.view_perfil);
+
+
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        professor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BatePapoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CalendarioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lupa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PesquisaActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
