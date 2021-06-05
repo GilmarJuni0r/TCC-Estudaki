46b70e321aa3aa68dde4edddcf22644268c30ca0
Karina Cruz <karinacss@outlook.com>
2021-06-05 17:59:30 -0300

    Backend da tela Me Ajuda + telaPerfil: Ajustar textos dentro do bloco de informacoes e centralizar textos + icon Me Ajuda na tela de Aulas e sua conexao com a tela Me Ajuda

--- app/src/main/java/nelson/aparecido/estudaki/MeAjudaActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/MeAjudaActivity.java
@@ -1,21 +1,98 @@
 package nelson.aparecido.estudaki;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jaeger.library.StatusBarUtil;
 
 public class MeAjudaActivity extends AppCompatActivity {
+
+    private Button btn1_txt, btn2_txt;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_me_ajuda);
 
         StatusBarUtil.setTransparent(this);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btn1_txt = (Button) findViewById(R.id.btn_pergunta1);
+        btn2_txt = (Button) findViewById(R.id.btn_pergunta2);
 
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+        if(usuarioAtual != null){
+            btn1_txt.setText("NÃO CONSIGO ACHAR A MATÉRIA");
+            btn1_txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    exibirConfirmacao("Per1WithLogin");
+                }
+            });
+
+            btn2_txt.setText("ONDE VEJO MINHA NOTA?");
+            btn2_txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    exibirConfirmacao("Per2WithLogin");
+                }
+            });
+        }
+        else{
+            btn1_txt.setText("NÃO TENHO LOGIN E NEM A SENHA");
+            btn1_txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    exibirConfirmacao("Per1WithoutLogin");
+                }
+            });
+            btn2_txt.setText("ESQUECI MINHA SENHA");
+            btn2_txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    exibirConfirmacao("Per2WithoutLogin");
+                }
+            });
+        }
+    }
+
+    public void exibirConfirmacao(String orientacao){
+        AlertDialog.Builder msgBox = new AlertDialog.Builder(this);
+        if (orientacao == "Per1WithoutLogin"){
+            msgBox.setTitle("NÃO TENHO LOGIN E NEM A SENHA");
+            msgBox.setMessage("ENTRE EM CONTATO COM A SECRETARIA DA SUA ESCOLA, E SOLICITE O SEU LOGIN E SENHA PARA ACESSO AO APLICATIVO ESTUDAKI.");
+            msgBox.show();
+        }else if(orientacao == "Per2WithoutLogin"){
+            msgBox.setTitle("ESQUECI MINHA SENHA");
+            msgBox.setMessage("ENTRE EM CONTATO COM A SECRETARIA DA SUA ESCOLA, E SOLICITE A NOVA SENHA.");
+            msgBox.show();
+        }else if(orientacao == "Per1WithLogin"){
+            msgBox.setTitle("NÃO CONSIGO ACHAR A MATÉRIA");
+            msgBox.setMessage("VÁ PARA GUIA MATÉRIAS E PROCURE A MATÉRIA QUE DESEJA;\n" +
+                    "\n" +
+                    "MATÉRIAS > PORTUGUÊS, MATEMÁTICA...\n" +
+                    "\n" +
+                    "CASO SUA MATÉRIA NÃO ESTEJA LÁ, CONTATE A SECRETARIA OU O PROFESSOR DA MATÉRIA ESPECIFICA.");
+            msgBox.show();
+        }else if(orientacao == "Per2WithLogin") {
+            msgBox.setTitle("ONDE VEJO MINHA NOTA?");
+            msgBox.setMessage("VÁ PARA GUIA NOTAS E PROCURE A NOTA DA MATÉRIA QUE DESEJA;\n" +
+                    "\n" +
+                    "NOTAS > PORTUGUÊS, MATEMÁTICA...\n" +
+                    "\n" +
+                    "CASO SUA NOTA NÃO ESTEJA LÁ, CONTATE O PROFESSOR DA MATÉRIA ESPECIFICA.");
+            msgBox.show();
+        }
     }
 }
