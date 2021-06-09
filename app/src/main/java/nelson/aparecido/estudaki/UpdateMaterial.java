cb713794145c3d3c0f2726a8f409a81e6edefc26
Nelson Aparecido <nelson.anre@gmail.com>
2021-06-09 20:17:49 -0300

    Duas telas de Update e adcionamento de Class e adicionamento no AndroidManifest.xml

---
+++ app/src/main/java/nelson/aparecido/estudaki/UpdateMaterial.java
@@ -0,0 +1,87 @@
+package nelson.aparecido.estudaki;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.jaeger.library.StatusBarUtil;
+
+public class UpdateMaterial extends AppCompatActivity {
+
+    private View calendario, lupa, home, professor, perfil, btn_me_ajuda;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_material);
+
+        barraDeTarefas();
+        StatusBarUtil.setTransparent(this);
+    }
+
+    private void barraDeTarefas() {
+        calendario = findViewById(R.id.view_calendario);
+        lupa = findViewById(R.id.view_lupa);
+        home = findViewById(R.id.view_home);
+        professor = findViewById(R.id.view_conversa_professor);
+        perfil = findViewById(R.id.view_perfil);
+
+        btn_me_ajuda = (View) findViewById(R.id.view_me_ajuda_calendario);
+        btn_me_ajuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeAjudaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        professor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), ContatosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), CalendarioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lupa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), PesquisaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
