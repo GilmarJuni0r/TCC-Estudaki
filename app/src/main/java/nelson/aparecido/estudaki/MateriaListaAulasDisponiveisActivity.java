cd1116d08d548b84b53ecaa7494896fa91cea7e1
gilmar <gilmaralmeidajr23@gmail.com>
2021-06-10 00:40:36 -0300

    funcaoAulaOnline

--- app/src/main/java/nelson/aparecido/estudaki/MateriaListaAulasDisponiveisActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/MateriaListaAulasDisponiveisActivity.java
@@ -1,22 +1,83 @@
 package nelson.aparecido.estudaki;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.jaeger.library.StatusBarUtil;
 
 public class MateriaListaAulasDisponiveisActivity extends AppCompatActivity {
 
-    private View calendario, lupa, home, professor, perfil, btn_me_ajuda;
+    private View calendario, lupa, home, professor, perfil, btn_me_ajuda, btnAulaAoVivo;
+    private ImageView btnUploadAula, btnEditaLink;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String usuarioID;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_lista_aulas_disponiveis);
         StatusBarUtil.setTransparent(this);
         barraDeTarefas();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        btnAulaAoVivo = findViewById(R.id.view_aulas_ao_vivo);
+        btnUploadAula = findViewById(R.id.btn_upload_aula);
+        btnEditaLink = findViewById(R.id.btn_edit_link);
+
+        DocumentReference refUser = db.collection("Usuario").document(usuarioID);
+        refUser.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                if(!(value.getString("ocupacao").equalsIgnoreCase("professor") || value.getString("ocupacao").equalsIgnoreCase("professora"))){
+                    btnEditaLink.setVisibility(View.INVISIBLE);
+                    btnUploadAula.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        btnAulaAoVivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DocumentReference documentReference = db.collection("Usuario").document(usuarioID);
+                documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                        DocumentReference refAux = db.collection("Turma").document(value.getString("turma"));
+                        refAux.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot docref, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                                String link = docref.getString("linkAula");
+                                if(!link.equalsIgnoreCase("")){
+                                    gotoURL(link);
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "Nenhum link para aulas ao vivo foi cadastrado", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
+        btnEditaLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TelaCadastroLinkAula.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void barraDeTarefas() {
@@ -75,4 +136,11 @@ public class MateriaListaAulasDisponiveisActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void gotoURL(String s) {
+
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
 }
+
