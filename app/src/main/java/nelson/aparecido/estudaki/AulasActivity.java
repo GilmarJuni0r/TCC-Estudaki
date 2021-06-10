31cd083bbaa190716488866c3a638a2d3521e7ab
Karina Cruz <karinacss@outlook.com>
2021-06-09 22:50:18 -0300

    Atualizacao label Materias da tela Aulas

--- app/src/main/java/nelson/aparecido/estudaki/AulasActivity.java
+++ app/src/main/java/nelson/aparecido/estudaki/AulasActivity.java
@@ -7,12 +7,18 @@ import android.view.View;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jaeger.library.StatusBarUtil;
 
 public class AulasActivity extends AppCompatActivity {
 
+    ConstraintLayout matematica, portugues, ciencias, geografia, historia;
     private View calendario, lupa, home, professor, perfil, btn_me_ajuda, aulas_matematica;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String usuarioID;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +26,66 @@ public class AulasActivity extends AppCompatActivity {
         setContentView(R.layout.tela_aulas);
         barraDeTarefas();
         StatusBarUtil.setTransparent(this);
+        selecionaMateria();
+    }
 
-        aulas_matematica = findViewById(R.id.view_tela_aulas_matematica);
+    private void selecionaMateria() {
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        aulas_matematica.setOnClickListener(new View.OnClickListener() {
+        //Ir para o menu da materia matematica
+        matematica = findViewById(R.id.constaint_aulas_matematica);
+        matematica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                db.collection("Usuario").document(usuarioID).update("materiaAtual", "Matemática");
                 Intent intent = new Intent(getApplicationContext(), MateriaListaAulasDisponiveisActivity.class);
                 startActivity(intent);
             }
         });
 
+        //Ir para o menu da materia portugues
+        portugues = findViewById(R.id.constaint_aulas_portugues);
+        portugues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Usuario").document(usuarioID).update("materiaAtual", "Português");
+                Intent intent = new Intent(getApplicationContext(), MateriaListaAulasDisponiveisActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        //Ir para o menu da materia ciencias
+        ciencias = findViewById(R.id.constaint_aulas_ciencias);
+        ciencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Usuario").document(usuarioID).update("materiaAtual", "Ciências");
+                Intent intent = new Intent(getApplicationContext(), MateriaListaAulasDisponiveisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Ir para o menu da materia geografia
+        geografia = findViewById(R.id.constaint_aulas_geografia);
+        geografia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Usuario").document(usuarioID).update("materiaAtual", "Geografia");
+                Intent intent = new Intent(getApplicationContext(), MateriaListaAulasDisponiveisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Ir para o menu da materia historia
+        historia = findViewById(R.id.constaint_aulas_historia);
+        historia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.collection("Usuario").document(usuarioID).update("materiaAtual", "História");
+                Intent intent = new Intent(getApplicationContext(), MateriaListaAulasDisponiveisActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void barraDeTarefas() {
