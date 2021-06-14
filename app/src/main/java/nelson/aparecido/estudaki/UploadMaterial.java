677bea646de10814d715bb8c1bfbc1ab7f9052e8
gilmar <gilmaralmeidajr23@gmail.com>
2021-06-14 20:30:02 -0300

    FuncaoUploadAulas

--- app/src/main/java/nelson/aparecido/estudaki/UploadMaterial.java
+++ app/src/main/java/nelson/aparecido/estudaki/UploadMaterial.java
@@ -1,5 +1,6 @@
 package nelson.aparecido.estudaki;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
@@ -12,9 +13,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -22,9 +26,12 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.jaeger.library.StatusBarUtil;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
@@ -50,10 +57,30 @@ public class UploadMaterial extends AppCompatActivity {
         cabecalho();
         barraDeTarefas();
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         editTitulo = findViewById(R.id.edit_titulo_arquivo_upload_material);
         editDescricao = findViewById(R.id.edit_descricao_arquivo_upload_material);
         btnSelecionaMaterial = findViewById(R.id.btn_seleciona_novo_material);
         btnUploadMaterial = findViewById(R.id.btn_upload_novo_material);
+
+        btnSelecionaMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DocumentReference documentReference = db.collection("Usuario").document(usuarioID);
+                documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                        if (value.getString("tipoArquivoAtual").equalsIgnoreCase("aulas")) {
+                            selecionaAula();
+                        }
+                        if (value.getString("tipoArquivoAtual").equalsIgnoreCase("Material")) {
+                            selecionaMaterial();
+                        }
+                    }
+                });
+            }
+        });
 
         btnUploadMaterial.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,32 +91,15 @@ public class UploadMaterial extends AppCompatActivity {
                     public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
 
                         if (!(editTitulo.getText().toString().isEmpty()) || !(editDescricao.getText().toString().isEmpty())) {
-                            if (selecionouArquivo) {
-
-                                storageReference = FirebaseStorage.getInstance().getReference(value.getString("tipoArquivoAtual"));
-                                String filename = UUID.randomUUID().toString();
-
-                                btnSelecionaMaterial.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        DocumentReference documentReference = db.collection("Usuario").document(usuarioID);
-                                        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
-                                                if (value.getString("tipoArquivoAtual").equalsIgnoreCase("aulas")) {
-                                                    selecionaAula();
-                                                }if (value.getString("tipoArquivoAtual").equalsIgnoreCase("Material")) {
-                                                    selecionaMaterial();
-                                                }
-                                            }
-                                        });
-                                    }
-                                });
-                                Toast.makeText(getApplicationContext(), "Material cadastrado com sucesso!", Toast.LENGTH_LONG).show();
-                                selecionouArquivo = false;
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Selecione uma aula para realizar upload", Toast.LENGTH_LONG).show();
+                            if(value.getString("tipoArquivoAtual").equalsIgnoreCase("aulas")){
+                                uploadAula();
                             }
+                            if(value.getString("tipoArquivoAtual").equalsIgnoreCase("material")){
+                                uploadMaterial();
+                            }
+                                //Toast.makeText(getApplicationContext(), "Material cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                                //selecionouArquivo = false;
+
                         } else {
                             Toast.makeText(getApplicationContext(), "Preencha os campos de título e descrição para cadastrar um novo material", Toast.LENGTH_LONG).show();
                         }//db.collection("Arquivos").document(value.).update("tipoArquivoAtual", "Atividade");
@@ -97,18 +107,16 @@ public class UploadMaterial extends AppCompatActivity {
                 });
             }
         });
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+        if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             diretorio = data.getData();
         }
-        if(requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+        if (requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             diretorio = data.getData();
         }
 
@@ -229,6 +237,73 @@ public class UploadMaterial extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Selecione o material"), PICK_FILE_REQUEST);
         selecionouArquivo = true;
+    }
+
+    private String uploadAula() {
+        final String[] saida = {""};
+        if (diretorio != null) {
+            String filename = UUID.randomUUID().toString();
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Realizando upload...");
+            progressDialog.show();
+            StorageReference aulaRef = storageReference.child("aulas/" + filename);
+
+            aulaRef.putFile(diretorio).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    saida[0] = aulaRef.getDownloadUrl().toString();
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Aula cadastrada com sucesso!", Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull @NotNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "Falha ao realizar upload de aula", Toast.LENGTH_LONG).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull @NotNull UploadTask.TaskSnapshot snapshot) {
+                    double progresso = (100.0 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                    progressDialog.setMessage(((int) progresso) + "% Carregado");
+
+                }
+            });
+        }
+        return saida[0];
+    }
+
+    private String uploadMaterial() {
+        final String[] saida = {""};
+        if (diretorio != null) {
+            String filename = UUID.randomUUID().toString();
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Realizando upload...");
+            progressDialog.show();
+            StorageReference materialRef = storageReference.child("materiais/" + filename);
+
+            materialRef.putFile(diretorio).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    saida[0] = materialRef.getDownloadUrl().toString();
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Material cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull @NotNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "Falha ao realizar upload de material", Toast.LENGTH_LONG).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull @NotNull UploadTask.TaskSnapshot snapshot) {
+                    double progresso = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                    progressDialog.setMessage(((int) progresso) + "% Carregado");
+
+                }
+            });
+        }
+
+        return saida[0];
     }
 }
 
